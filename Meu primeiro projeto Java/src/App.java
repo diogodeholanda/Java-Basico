c0de200ebca03e8379386dfc843602bedacfbc1e
diogodeholanda <diogodeholanda@hotmail.com>
2023-01-21 22:07:07 -0300

    commit 5

--- Meu primeiro projeto Java/src/App.java
+++ Meu primeiro projeto Java/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("OlHello, World!");
+        System.out.println("Oi, World!");
+        //https://www.youtube.com/watch?v=vim1bTa-Bkc
     }
 }
